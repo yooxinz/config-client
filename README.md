65a09cd0b94b77fa2891a816aaaa01753da3daac
yooxinz <zhangyongxing89@gmail.com>
2018-08-21 10:02:13 +0800

    [IMP] update cloud D verison and add eureka discover

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ spring cloud config client
 
 spring.cloud.config.uri 必须配置在bootstrap中
 
-启动该服务前需要先启动[config server](https://github.com/yooxinz/config-server) 
+启动该服务前需要先启动
+[config server](https://github.com/yooxinz/config-server)
+[eureka-server](https://github.com/yooxinz/eureka-server)
--- src/main/java/yooxinz/springcloud/ConfigClientApplication.java
+++ src/main/java/yooxinz/springcloud/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package yooxinz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
